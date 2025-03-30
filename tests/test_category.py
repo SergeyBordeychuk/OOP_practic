8d8ac4a7e75eac110fdd86c0f91095b1bc6751ab
Sergey Bordeychuk <twnue2020@gmail.com>
2025-03-30 21:54:40 +0300

    work 14.2

--- tests/test_category.py
+++ tests/test_category.py
@@ -10,3 +10,13 @@ def test_init_category(category_fruits):
 def test_counts(category_fruits, category_weapon):
     assert Category.product_count == 4
     assert Category.category_count == 3
+
+
+def test_getter(category_fruits, category_weapon):
+    assert category_fruits.products == ["Banan, 100 руб. Остаток: 125 шт."]
+    assert category_weapon.products == ["Sword, 1000 руб. Остаток: 10 шт.", "Katana, 2500 руб. Остаток: 5 шт."]
+
+
+def test_add_product(category_fruits, apple):
+    category_fruits.add_product(apple)
+    assert category_fruits.products == ["Banan, 100 руб. Остаток: 125 шт.", "Apple, 120 руб. Остаток: 100 шт."]
